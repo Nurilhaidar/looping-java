6d692bf00d3085cd370d4727c4bdece67f72885c
Nuril <Nuril@LAPTOP-R1AG4U4Q>
2018-08-02 11:14:06 +0700

    for, while, do while

---
+++ src/j08/Contoh1.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package j08;
+
+/**
+ *
+ * @author Nuril
+ */
+public class Contoh1 {
+    public static void main(String[] args) {
+        
+        //Perulangan For
+        int x;
+        for (x = 1;x <= 5;x++)
+        {
+            System.out.println("SMK Telkom Malang");
+        }
+        
+        //Perulangan While
+        int y = 1;
+        while (y <= 5)
+        {
+            System.out.println("SMK Telkom Malang");
+            y++;
+        }
+        
+        //Perulangan Do While
+        int z = 1;
+        do
+        {
+            System.out.println("SMK Telkom Malang");
+            z++;
+        }
+        while(z <= 5);
+    }
+}
